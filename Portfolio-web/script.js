5642b2405ce7a5201517acf06a60f91ed64c8381
Fasih Khan <149299341+FasihKhan1229@users.noreply.github.com>
2024-10-21 11:45:41 -0700

    Add files via upload

---
+++ Portfolio-web/script.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#typed',{
+    strings:["Frontend Developer", "Software Engineer", "Data Scientist", "Product Manager"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:100,
+    loop:true,
+    cursorChar: "|",
+    
+})
